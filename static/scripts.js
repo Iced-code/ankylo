00dd5816f0ee7671ae6d0e510e700eccba0d68ad
iced-code github <ayaanmodak1@gmail.com>
2026-03-04 07:01:37 -0500

    website updates

--- static/scripts.js
+++ static/scripts.js
@@ -1,4 +1,11 @@
 function setTheme(){
     document.body.classList.toggle("light")
     document.body.classList.toggle("dark")
+
+    if (document.body.classList.contains("light")){
+        document.getElementById("themeButton").innerHTML = "&#x263E;";
+    }
+    else {
+        document.getElementById("themeButton").innerHTML = "&#x2600;";
+    }
 }
